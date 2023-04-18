b59b8cd76605f79bae08d79bbed53c7bb23ea113
hotdoggey <biser951@gmail.com>
2023-04-18 23:09:46 +0100

    ff

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page_footer">
+        <span class="footer_content">{{ footer_content }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [`footer_content`],
+};
+</script>
+
+<style scoped>
+.page_footer {
+    display: flex;
+    padding: 0pt 10pt 0pt 30pt;
+    background: var(--primary-color);
+    color: whitesmoke;
+}
+.page_footer > * {
+    /* border: 15px solid orange;  <<< uncomment this to see effect of *star* */
+    /* border: 15px solid orange; */
+    margin: 15px;
+}
+
+.footer_content {
+    height: 40%;
+    margin-top: auto;
+    margin-bottom: auto;
+}
+</style>
